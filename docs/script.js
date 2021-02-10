35a4e6c3d8d77a0ade89aa35e4e5a57accd6baef
VitorAntonioMezzalira <62727984+VitorAntonioMezzalira@users.noreply.github.com>
2021-02-10 17:47:31 -0300

    ...

--- docs/script.js
+++ docs/script.js
@@ -2,37 +2,23 @@ function Invoice(id, money, portions) {
     this.id = id;
     this.value = money;
     this.portions = portions;
-    this.discount = function() {
+    this.discount = function () {
         let discount = 0;
         this.portions.forEach(portion => {
             discount += portion.value;
         });
         return discount
     };
-    this.rest = function() {
+    this.rest = function () {
         return this.value - this.discount();
     };
+    this.valueVitor = function () {
+        let total = (this.rest() / 4);
+        this.portions.forEach(portion => {
+            portion
+        })
+    }
 };
-
-document.querySelector('#button-open-invoice').addEventListener('click', () => {
-    const value = document.querySelector('#total-money').value;
-    invoices.push(new Invoice(invoices.length, value, []));
-    document.querySelector('#key').value = invoices[invoices.length - 1].id;
-    openForm();
-});
-
-// SUBMIT EVENT
-document.querySelector('.portions-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const id = document.querySelector('#key').value;
-
-    const data = getData();
-    if(data) invoices[id].portions.push(data);
-
-    refreshInvoiceInfo(id);
-
-    handleMoney(true, id)
-});
 
 // INFO
 function refreshInvoiceInfo(id) {
@@ -51,15 +37,15 @@ function getData() {
 
     const data = {
         value: Number((document.getElementById('money').value).replace(',', '.')),
-        check: [
-            document.getElementById('checkbox-vitor').checked,
-            document.getElementById('checkbox-debora').checked,
-            document.getElementById('checkbox-samanta').checked,
-            document.getElementById('checkbox-fernanda').checked
-        ]
+        check: {
+            vitor: document.getElementById('checkbox-vitor').checked,
+            debora: document.getElementById('checkbox-debora').checked,
+            fernanda: document.getElementById('checkbox-samanta').checked,
+            samanta: document.getElementById('checkbox-fernanda').checked
+        }
     };
 
-    if (data.check[0] || data.check[1] || data.check[2] || data.check[3]) {
+    if (data.check.vitor || data.check.debora || data.check.fernanda || data.check.samanta) {
         return data
     };
 
@@ -67,37 +53,28 @@ function getData() {
 
 };
 
-// FILLS THE TRUE CELLS WITH THE RESPECTIVE AMOUNT OF MONEY
-function fillCells(rows) {
-    rows.forEach(row => {
+// Choose invoice
+document.querySelector('#button-choose-invoice').addEventListener('click', () => {
+    const id = document.querySelector('#key').value;
+    handleInvoiceTable(id)
+});
 
-        const moneyRow = row.querySelector('.cell-money').innerText;
-        const trueCells = row.querySelectorAll('.true');
-        const moneyCell = (moneyRow / trueCells.length).toFixed(2);
+// Create invoice
+document.querySelector('#button-create-invoice').addEventListener('click', () => {
+    const value = document.querySelector('#total-money').value;
+    invoices.push(new Invoice(invoices.length, value, []));
+    document.querySelector('#key').value = invoices[invoices.length - 1].id;
+    openForm();
+});
 
-        trueCells.forEach(cell => {
-            cell.innerText = moneyCell;
-        });
-    });
-};
+// Create Portion of invoice
+document.querySelector('.portions-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = document.querySelector('#key').value;
 
+    const data = getData();
+    if (data) invoices[id].portions.push(data);
 
-function handleMoney(isSubmit, id) {
-
-    table.innerHTML = '';
-
-    createTableHeader();
-
-    createRow({ value: document.querySelector('#rest-invoice').value, check: [true, true, true, true] });
-
-    invoices[id].portions.forEach((content, i) => {
-        if (content) {
-            createRow(content, i);
-        };
-    });
-
-    const rows = document.querySelectorAll('.portions-table tr.body-row');
-    fillCells(rows);
-    createLastRow(rows);
-
-};
+    refreshInvoiceInfo(id);
+    handleInvoiceTable(id);
+});
--- docs/tableBuilder.js
+++ docs/tableBuilder.js
@@ -1,8 +1,8 @@
-const headersNames = ['Valor', 'Vitor', 'Debora', 'Samanta', 'Fernanda'];
-
 // CREATE TABLE HEADER
 function createTableHeader() {
-    
+
+    const headersNames = ['Valor', 'Vitor', 'Debora', 'Fernanda', 'Samanta'];
+
     const firstRow = document.createElement('TR');
 
     for (let i = 0; i < headersNames.length; i++) {
@@ -52,16 +52,15 @@ function createRow(contents, i) {
     row.appendChild(moneyCell);
     row.setAttribute('class', 'body-row')
 
-    contents.check.forEach(isCheck => {
+    const names = ['vitor', 'debora', 'fernanda', 'samanta']
 
-        const cell = document.createElement('TD');
-
-        if (isCheck === true) cell.setAttribute('class', 'true');
-        else cell.setAttribute('class', 'false')
-
+    //
+    for (let i = 0; i < 4; i++) {
+        let cell = document.createElement('TD');
+        if (contents.check[names[i]]) cell.setAttribute('class', 'true');
+        else cell.setAttribute('class', 'false');
         row.appendChild(cell);
-
-    });
+    }
 
     const deleteCell = document.createElement('TD');
     deleteCell.setAttribute('key', i)
@@ -78,5 +77,39 @@ function createRow(contents, i) {
     row.appendChild(deleteCell)
 
     table.appendChild(row)
+
+};
+
+// FILLS THE TRUE CELLS WITH THE RESPECTIVE AMOUNT OF MONEY
+function fillCells(rows) {
+    rows.forEach(row => {
+
+        const moneyRow = row.querySelector('.cell-money').innerText;
+        const trueCells = row.querySelectorAll('.true');
+        const moneyCell = (moneyRow / trueCells.length).toFixed(2);
+
+        trueCells.forEach(cell => {
+            cell.innerText = moneyCell;
+        });
+    });
+};
+
+function handleInvoiceTable(id) {
+
+    table.innerHTML = '';
+
+    createTableHeader();
+
+    createRow({ value: invoices[id].rest(), check: { vitor: true, debora: true, fernanda: true, samanta: true } });
+
+    invoices[id].portions.forEach((content, i) => {
+        if (content) {
+            createRow(content, i);
+        };
+    });
+
+    const rows = document.querySelectorAll('.portions-table tr.body-row');
+    fillCells(rows);
+    createLastRow(rows);
 
 };
